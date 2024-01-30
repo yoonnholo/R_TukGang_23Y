aea13fdf73f01e9bab703b55a39b7231ee64d3ae
yoonnholo <sbstony@snu.ac.kr>
2024-01-30 21:14:36 +0900

    first

---
+++ My git project.R
@@ -0,0 +1,24 @@
+AA<-500
+##https://github.com/yoonnholo/Myuri
+
+library(ggplot2)
+library(tidyverse)
+library(extrafont)
+library(haven)
+library(car)
+library(corrplot)
+library(rstan)
+theme_set(theme_minimal(base_family = "Malgun Gothic"))
+
+getwd()
+R.version
+
+font_import(pattern = "malgun")
+y
+theme_set(theme_minimal(base_family = "malgun"))
+
+fonts()
+
+install.packages("haven")
+
+install.packages("corrplot")
